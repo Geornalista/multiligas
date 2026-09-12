b1d9f9db5139601d3327cc9c606052794f951fa9
Geornalista <83824843+Geornalista@users.noreply.github.com>
2026-09-12 09:21:26 -0300

    Update update-all.js

--- update-all.js
+++ update-all.js
@@ -32,8 +32,7 @@ const LEAGUES = {
     noruega:     { id: 59,  name: "Noruega" },
     suica:       { id: 69,  name: "Suíça" },
     suecia:      { id: 67,  name: "Suécia" },
-    australia:   { id: 113, name: "Austrália" },
-    china:       { id: 120, name: "China"}
+    australia:   { id: 113, name: "Austrália" }
 };
 
 function ensureDir(dir) {
